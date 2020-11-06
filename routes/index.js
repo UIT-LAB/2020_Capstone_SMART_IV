15fd409e6966f52a62103eb5937ad558c2585bd7
kyj970513 <kyj970513@naver.com>
2020-11-06 14:57:48 +0900

    Add notice router & Connect DB from oq and faq

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../config/KYJ_db");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.query(`SELECT * FROM notice`, function (error, db_data) {
+      if (error) {
+          throw error;
+      }
+      console.log(db_data)
+      res.render('index', { db_data });
+  });
 });
 
 module.exports = router;
